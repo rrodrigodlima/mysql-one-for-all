561476e3348801b61f888825dea4b356d2c0d2b0
rrodrigodlima <rdrigo019@gmail.com>
2023-04-18 16:10:51 -0300

    requisito 3

---
+++ challenges/desafio3.sql
@@ -0,0 +1,11 @@
+SELECT 
+	usuario.nome AS pessoa_usuaria,
+  COUNT(historico.date) AS musicas_ouvidas,
+  ROUND(SUM(musica.duracao)/ 60, 2) AS total_minutos
+FROM user AS usuario
+INNER JOIN reproduzido AS historico
+ON usuario.user_id = historico.user_id
+INNER JOIN musica AS musica
+ON musica.musica_id = historico.musica_id
+GROUP BY usuario.nome
+ORDER BY usuario.nome;
